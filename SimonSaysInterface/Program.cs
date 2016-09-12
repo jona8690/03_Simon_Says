95f6ace5ececf382f596d82f2a0c766f41b4f750
jona8690 <jona8690@edu.eal.dk>
2016-09-12 10:44:29 +0200

    Program now uses simon class

--- SimonSaysInterface/Program.cs
+++ SimonSaysInterface/Program.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using _03_Simon_Says;
 
 namespace SimonSaysInterface {
 	class Program {
 		static void Main(string[] args) {
+			Simon simon = new Simon();
+
+			string Input = Console.ReadLine();
+
+			Console.WriteLine(simon.Echo(Input));
 		}
 	}
 }
