edcf96510d0fc5199bf2cb4d61a1f0f4917f128f
jona8690 <jona8690@edu.eal.dk>
2016-09-12 10:39:34 +0200

    Updated methods and classes to public

--- 03_Simon_Says/Simon.cs
+++ 03_Simon_Says/Simon.cs
@@ -6,34 +6,29 @@ using System.Collections.Generic;
 
 namespace _03_Simon_Says
 {
-    internal class Simon
+    public class Simon
     {
-        internal string Echo(string v)
+        public string Echo(string v)
         {
             return v.ToLower();
         }
 
-        internal string Shout(string v)
+        public string Shout(string v)
         {
             return v.ToUpper();
         }
 
-        internal string Repeat(string v, int r = 2)
+        public string Repeat(string v, int r = 2)
         {
             return string.Join(" ", Enumerable.Repeat(v,r));
         }
 
-        /*internal object Repeat(string v1, int v2)
-        {
-            throw new NotImplementedException();
-        }*/
-
-        internal string StartOfWord(string v1, int v2)
+        public string StartOfWord(string v1, int v2)
         {
             return v1.Substring(0, v2);
         }
 
-        internal string FirstWord(string v)
+        public string FirstWord(string v)
         {
             string[] fw;
             char[] space = { ' ' };
@@ -41,7 +36,7 @@ namespace _03_Simon_Says
             return fw[0];
         }
 
-        internal string Titleize(string v)
+       public string Titleize(string v)
         {
             CultureInfo cultureInfo = Thread.CurrentThread.CurrentCulture;
             TextInfo textInfo = cultureInfo.TextInfo;
