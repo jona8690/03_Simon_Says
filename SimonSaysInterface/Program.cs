f0578c7fa4414f56b15053b9ce7577d44b3a6179
jona8690 <jona8690@edu.eal.dk>
2016-09-12 11:07:41 +0200

    Started Console Application Added commands: - echo - shout - repeat

--- SimonSaysInterface/Program.cs
+++ SimonSaysInterface/Program.cs
@@ -10,9 +10,57 @@ namespace SimonSaysInterface {
 		static void Main(string[] args) {
 			Simon simon = new Simon();
 
+			Start:
+
 			string Input = Console.ReadLine();
 
-			Console.WriteLine(simon.Echo(Input));
+			char[] space = { ' ' };
+			string[] cmd = Input.Split(space);
+
+			switch(cmd[0]) {
+				default:
+					Console.WriteLine("Unknow Command. Use !commands to show all.");
+					break;
+
+				case "!commands":
+					ShowCommands();
+					break;
+
+				case "echo":
+					Console.WriteLine(simon.Echo(cmd[1]));
+					break;
+
+				case "shout":
+					Console.WriteLine(simon.Shout(cmd[1]));
+					break;
+
+				case "repeat":
+					int Repeat = Int32.Parse(cmd[2]);
+					Console.WriteLine(simon.Repeat(cmd[1], Repeat));
+					break;
+			}
+
+			EndProgram();
+			goto Start;
+		}
+
+		private static void ShowCommands() {
+			List<string> Commands = new List<string>();
+
+			Commands.Add("echo <input> - Repat what you enter");
+			Commands.Add("shout <input> - Shout what you enter");
+			Commands.Add("repeat <input> <times> - Repat what you enter");
+
+			foreach (string write in Commands) {
+				Console.WriteLine(write);
+			}
+		}
+
+		private static void EndProgram() {
+			Console.WriteLine("");
+			Console.WriteLine("Press any key to continue...");
+			Console.ReadKey();
+			Console.Clear();
 		}
 	}
 }
